be34337549131cbc73685abb5dac7f04eb731f3f
kaleavarajuro-beep <kaleavarajuro@gmail.com>
2026-05-02 13:18:47 +0700

    2 mei

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 
 # Inisialisasi semua modul pygame
 pygame.init()
@@ -41,19 +42,52 @@ player_rect = player.get_rect(midbottom=(80, 300))
 
 # Variabel gravitasi untuk pemain (digunakan saat lompat)
 player_gravity = 0
+jump_sound = pygame.mixer.Sound("gallery/audio/jump.mp3")
+back_sound = pygame.mixer.Sound("gallery/audio/backsound.mp3")
+back_sound.play(loops=-1)
+back_sound.set_volume(0.5)
 
 # Memuat latar belakang dan tanah
 skybox = pygame.image.load('gallery/sprites/Sky.png').convert()
 ground = pygame.image.load('gallery/sprites/Ground.png').convert()
 
+#Video frames for skeleton
+enemy_frame1 = pygame.image.load("gallery/sprites/enemies/Enemy.png").convert_alpha()
+enemy_frame2 = pygame.image.load("gallery/sprites/enemies/Enemy2.png").convert_alpha()
+enemy_frames = [enemy_frame1, enemy_frame2]
+enemy_frame_index = 0
+enemy = enemy_frames[enemy_frame_index]
+
+#Video frames for ghost
+enemy2_frame1 = pygame.image.load("gallery/sprites/enemies/Enemy2.png").convert_alpha()
+enemy2_frame2 = pygame.image.load("gallery/sprites/enemies/Enemy2_2.png").convert_alpha()
+enemy2_frames = [enemy2_frame1, enemy2_frame2]
+enemy2_frame_index = 0
+enemy2 = enemy2_frames[enemy2_frame_index]
+
+obstacle_rect_list = []
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer, 1000)
+
+enemy_animation_timer = pygame.USEREVENT + 2
+pygame.time.set_timer(enemy_animation_timer, 200)
+
+enemy2_animation_timer = pygame.USEREVENT + 3
+pygame.time.set_timer(enemy2_animation_timer, 500)
+
 # Fungsi untuk menampilkan permainan saat aktif
 def active_game():
-    global player_gravity 
+    global player_gravity, obstacle_rect_list, player_rect
     window_screen.blit(skybox, (0,0))         # Tampilkan background
     window_screen.blit(ground, (0, 320))      # Tampilkan tanah/ground
-    score = display_score()                   # Tampilkan skor saat ini
+    score = display_score()
+    player_gravity += 1
+    player_rect.y += player_gravity
+    if player_rect.bottom >= 320:
+        player_rect.bottom = 300                   # Tampilkan skor saat ini
     player_animation()                        # Jalankan animasi pemain
     window_screen.blit(player, player_rect)   # Tampilkan pemain di layar
+    obstacle_rect_list = obstacle_movement(obstacle_rect_list)
 
 # Fungsi untuk menampilkan layar saat permainan belum dimulai / tidak aktif
 def inactive_game():
@@ -81,13 +115,50 @@ def display_score():
     window_screen.blit(score, score_rect)  # Tampilkan skor
     return current_time  # Kembalikan nilai skor
 
+def obstacle_movement(obstacle_list):
+    if obstacle_list:
+        for obstacle_rect in obstacle_list:
+            obstacle_rect.x -= 5
+            if obstacle_rect.bottom == 320:
+                window_screen.blit(enemy, obstacle_rect)
+            else:
+                window_screen.blit(enemy2, obstacle_rect)
+        obstacle_list = [obstacle for obstacle in obstacle_list if obstacle.x > -100]
+        return obstacle_list
+    else:
+        return []
+
+def spawn_enemy():
+    global enemy_frame_index, enemy2_frame_index, enemy2_frame2, enemy, enemy2
+    if event.type == obstacle_timer:
+        if randint(0, 2):
+            print('Enemy has been spawned')
+            obstacle_rect_list.append(enemy.get_rect(bottomright =(randint(900, 1100), 320)))
+        else:
+            obstacle_rect_list.append(enemy2.get_rect(bottomright =(randint(900, 1100), 210)))
+    if event.type == enemy_animation_timer:
+        if enemy_frame_index == 0:
+            enemy_frame_index = 1
+        else:
+            enemy_frame_index = 0
+        enemy = enemy_frames[enemy_frame_index]
+    
+    if event.type == enemy2_animation_timer:
+        if enemy2_frame_index == 0:
+            enemy2_frame_index = 1
+        else:
+            enemy2_frame_index = 0
+        enemy2 = enemy2_frames[enemy2_frame_index]
 # Fungsi animasi pemain agar terlihat berjalan
 def player_animation():
     global player_index, player
-    player_index += 0.1  # Tambah indeks sedikit demi sedikit untuk animasi halus
-    if player_index >= len(player_walk):  # Jika melebihi jumlah frame animasi, reset ke 0
-        player_index = 0
-    player = player_walk[int(player_index)]  # Update gambar pemain
+    if player_rect.bottom < 300:
+        player = player_jump
+    else:
+        player_index += 0.1
+        if player_index >= len(player_walk):
+            player_index = 0
+        player = player_walk[int(player_index)]
 
 # Loop utama game
 while True:
@@ -98,8 +169,13 @@ while True:
             exit()
 
         if game_active:
-            # Jika game sedang aktif, tidak ada aksi khusus di sini
-            print("Game Active")
+            spawn_enemy()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                    jump_sound.play()
+                    player_gravity = -20
+                # Jika game sedang aktif, tidak ada aksi khusus di sini
+                print("Game Active")
         else:
             # Jika game belum aktif, mulai game saat tombol spasi ditekan
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
